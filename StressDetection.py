7c8f971b296cfc268b0c705beaa7188ff27c21c6
Maimoona Khilji <64362437+MaimoonaKhilji@users.noreply.github.com>
2023-07-24 02:19:58 +0500

    Update StressDetection.py

--- StressDetection.py
+++ StressDetection.py
@@ -7,8 +7,8 @@ from tensorflow.keras.models import load_model
 def stress_prediction(input_data):
     loaded_model = load_model('NN_model.h5')
     input_data_as_numpy_array = np.asarray(input_data)
-   # input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    prediction = loaded_model.predict(input_data_as_numpy_array)
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+    prediction = loaded_model.predict(input_data_reshaped)
     return prediction[0][0]
 
 def main():
@@ -20,7 +20,7 @@ def main():
     Pulse_rate = st.text_input("Pulse rate")
 
     if st.button("Stress Prediction"):
-        input_data = [Gender, Age, Bmi, Temperature, Pulse_rate]
+        input_data = [Gender, Age, Temperature, Pulse_rate,Bmi]
         prediction = stress_prediction(input_data)
         st.success(f"Stress Level: {prediction}")
 

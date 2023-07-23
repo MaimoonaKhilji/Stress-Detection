4cd985c90e6ce2a1eac3b122b474580107f77a02
Maimoona Khilji <64362437+MaimoonaKhilji@users.noreply.github.com>
2023-07-24 02:23:09 +0500

    Update StressDetection.py

--- StressDetection.py
+++ StressDetection.py
@@ -4,11 +4,23 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 
+# Define the deep learning model architecture
+def create_model():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(5, activation='relu', input_shape=(5,)))
+    model.add(tf.keras.layers.Dense(5, activation='relu'))
+    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
 def stress_prediction(input_data):
-    loaded_model = tf.keras.models.load_model('NN_model.h5')
-    input_data_as_numpy_array = np.asarray(input_data)
+    # Load the model using the custom architecture function
+    model = create_model()
+    model.load_weights('NN_model.h5')
+
+    input_data_as_numpy_array = np.asarray(input_data, dtype=float)
     input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    prediction = loaded_model.predict(input_data_reshaped)
+    prediction = model.predict(input_data_reshaped)
     return prediction[0][0]
 
 def main():
@@ -20,7 +32,7 @@ def main():
     Pulse_rate = st.text_input("Pulse rate")
 
     if st.button("Stress Prediction"):
-        input_data = [Gender, Age, Temperature, Pulse_rate, Bmi]
+        input_data = [Gender, Age, Bmi, Temperature, Pulse_rate]
         prediction = stress_prediction(input_data)
         st.success(f"Stress Level: {prediction}")
 

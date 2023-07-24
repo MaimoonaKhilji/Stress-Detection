4311f02a5099b6464b47c3e001920758567b2ec7
Maimoona Khilji <64362437+MaimoonaKhilji@users.noreply.github.com>
2023-07-25 00:35:16 +0500

    Update StressDetection.py

--- StressDetection.py
+++ StressDetection.py
@@ -94,52 +94,56 @@ def main():
            # st.info("No Stress Detected") 
         else:
             result="Stress Detected"
-             # Create a sidebar for notes or additional content
-            st.sidebar.markdown("### Practices to Relax Stress")
-            st.sidebar.write("""
-            - Deep Breathing: Practice deep breathing exercises to calm the nervous system and reduce stress levels. Take slow, deep breaths, hold briefly, and then exhale slowly.
-            - Meditation: Engage in mindfulness meditation to focus your mind on the present moment and let go of stressful thoughts.
-            - Progressive Muscle Relaxation: Tense and then relax each muscle group in your body, starting from your feet up to your head, to release physical tension.
-            - Yoga: Participate in yoga classes or follow guided yoga sessions to improve flexibility, relieve tension, and promote relaxation.
-            - Exercise: Regular physical activity, such as walking, jogging, or swimming, can help release endorphins and reduce stress.
-            
-            - Spending Time in Nature: Take a walk in the park, hike, or simply spend time outdoors to connect with nature and promote a sense of calm.
-            
-            - Journaling: Write down your thoughts, feelings, and worries in a journal to gain clarity and release emotional tension.
-            
-            - Reading: Escape into a good book or engage in literature that interests you to take your mind off stressors.
-            
-            - Creative Outlets: Engage in creative activities like drawing, painting, crafting, or playing a musical instrument to express yourself and unwind.
-            
-            - Listening to Music: Listen to soothing music or your favorite tunes to relax and uplift your mood.
-            
-            - Socializing: Spend quality time with friends and loved ones to share experiences and receive emotional support.
-            
-            - Laughter: Watch a comedy show or engage in activities that make you laugh, as laughter can release endorphins and reduce stress.
-            
-            - Aromatherapy: Use essential oils like lavender, chamomile, or eucalyptus to promote relaxation and reduce stress.
-            
-            - Limiting Screen Time: Reduce exposure to screens (phones, computers, TVs) before bedtime to improve sleep quality and reduce stress.
-            
-            - Mindful Eating: Pay attention to your meals, savoring each bite and eating healthy, balanced foods that nourish your body.
-            
-            - Warm Baths: Take a warm bath with Epsom salts or essential oils to soothe your muscles and calm your mind.
-            
-            - Visualization: Imagine yourself in a peaceful and serene place to evoke relaxation responses.
-            
-            - Mindful Walking: Practice walking meditation, paying attention to eachSl step and your surroundings.
-            
-            - Disconnect: Take a break from technology and social media to reduce mental clutter and promote relaxation.
-            
-            - **Seek Professional Help:** If stress becomes overwhelming or chronic, don't hesitate to seek support from a mental health professional or counselor.
-            """)
+
+             if st.button("Practices to Relax Stress"):
+                 # Create a sidebar for notes or additional content
+                st.sidebar.markdown("### Practices to Relax Stress")
+                st.sidebar.write("""
+                - Deep Breathing: Practice deep breathing exercises to calm the nervous system and reduce stress levels. Take slow, deep breaths, hold briefly, and then exhale slowly.
+                - Meditation: Engage in mindfulness meditation to focus your mind on the present moment and let go of stressful thoughts.
+                - Progressive Muscle Relaxation: Tense and then relax each muscle group in your body, starting from your feet up to your head, to release physical tension.
+                - Yoga: Participate in yoga classes or follow guided yoga sessions to improve flexibility, relieve tension, and promote relaxation.
+                - Exercise: Regular physical activity, such as walking, jogging, or swimming, can help release endorphins and reduce stress.
                 
-            st.write("Stress Detected! Playing Calm Sound...")
-         #   calm_sound_path = 'Meydan-Freezing-but-warm.mp3'
-            st.audio('Meydan-Freezing-but-warm.mp3', format='audio/mp3')
-            st.audio('Moon-Dock.mp3', format='audio/mp3')
-            st.audio('scott-buckley-jul.mp3', format='audio/mp3')
-            st.audio('please-calm-my-mind-125566.mp3', format='audio/mp3')
+                - Spending Time in Nature: Take a walk in the park, hike, or simply spend time outdoors to connect with nature and promote a sense of calm.
+                
+                - Journaling: Write down your thoughts, feelings, and worries in a journal to gain clarity and release emotional tension.
+                
+                - Reading: Escape into a good book or engage in literature that interests you to take your mind off stressors.
+                
+                - Creative Outlets: Engage in creative activities like drawing, painting, crafting, or playing a musical instrument to express yourself and unwind.
+                
+                - Listening to Music: Listen to soothing music or your favorite tunes to relax and uplift your mood.
+                
+                - Socializing: Spend quality time with friends and loved ones to share experiences and receive emotional support.
+                
+                - Laughter: Watch a comedy show or engage in activities that make you laugh, as laughter can release endorphins and reduce stress.
+                
+                - Aromatherapy: Use essential oils like lavender, chamomile, or eucalyptus to promote relaxation and reduce stress.
+                
+                - Limiting Screen Time: Reduce exposure to screens (phones, computers, TVs) before bedtime to improve sleep quality and reduce stress.
+                
+                - Mindful Eating: Pay attention to your meals, savoring each bite and eating healthy, balanced foods that nourish your body.
+                
+                - Warm Baths: Take a warm bath with Epsom salts or essential oils to soothe your muscles and calm your mind.
+                
+                - Visualization: Imagine yourself in a peaceful and serene place to evoke relaxation responses.
+                
+                - Mindful Walking: Practice walking meditation, paying attention to eachSl step and your surroundings.
+                
+                - Disconnect: Take a break from technology and social media to reduce mental clutter and promote relaxation.
+                
+                - **Seek Professional Help:** If stress becomes overwhelming or chronic, don't hesitate to seek support from a mental health professional or counselor.
+                """)
+
+            if st.button("Do you want to listen Calm music?"):
+                
+                st.write("Stress Detected! Playing Calm Sound...")
+             #   calm_sound_path = 'Meydan-Freezing-but-warm.mp3'
+                st.audio('Meydan-Freezing-but-warm.mp3', format='audio/mp3')
+                st.audio('Moon-Dock.mp3', format='audio/mp3')
+                st.audio('scott-buckley-jul.mp3', format='audio/mp3')
+                st.audio('please-calm-my-mind-125566.mp3', format='audio/mp3')
         st.success(f"Stress Level: {result}")
 
 

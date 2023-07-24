c76e9e8a91299e91b81d0695f5a3c88a924b48aa
Maimoona Khilji <64362437+MaimoonaKhilji@users.noreply.github.com>
2023-07-24 23:50:59 +0500

    Update project.py

--- project.py
+++ project.py
@@ -1,13 +1,10 @@
-# app.py
-
 import streamlit as st
 
-def main():
-    # Load your custom CSS file
-    st.markdown('<link href="custom.css" rel="stylesheet">', unsafe_allow_html=True)
+# Add the CSS file to the app
+with open("custom.css", "r") as f:
+    css = f.read()
+    st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
 
-    st.title('Custom Theme Example')
-    st.write('This is a Streamlit app with a custom theme!')
-
-if __name__ == '__main__':
-    main()
+# Use the CSS styles in the app
+st.title("My Beautiful App")
+st.write("This is a beautiful app.")
